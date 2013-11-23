66dbd265167e02c7302903983352a2ae5b6e4010
gryte <alinkous@gmail.com>
2013-11-23 14:44:39 -0600

    added template.cmd

---
+++ template.cmd
@@ -0,0 +1,16 @@
+REM=============================================================================
+REM	Filename:	template.cmd
+REM	Author:		Adam Linkous
+REM	Creation Date:	yyyy-mm-dd
+REM	Last Update	yyyy-mm-dd
+REM	Version:	major.minor.build
+REM	Description:	Descriptions go here
+REM	URL:		URL's go here
+REM=============================================================================
+
+REM Declare variables
+
+REM Start code
+
+REM Exit script
+EXIT
